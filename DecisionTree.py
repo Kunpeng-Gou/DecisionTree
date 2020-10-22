33651d38eebc98ac919fd2344fe94d254a023387
Kunpeng-Gou <1403916395@qq.com>
2020-10-22 22:28:19 +0800

    start

---
+++ DecisionTree.py
@@ -0,0 +1,12 @@
+
+
+class DecisionTree(object):
+    def __init__(self):
+        pass
+
+    def predict(self, arr):
+        return 0
+
+
+if __name__ == '__main__':
+    print('finished')
---
+++ test.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+data = pd.read_csv('diabetes.csv')
+
+print(data)
+print(data.info())
+print(data.values)
